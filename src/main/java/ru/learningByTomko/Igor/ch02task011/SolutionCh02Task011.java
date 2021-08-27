4b16953f42750591db4cd2e58d0770f6a7ef8790
BulbaBeggins <Bumerang>
2021-08-27 21:51:56 +0400

    Создан класс решения задачи SolutionCh02Task011

---
+++ src/main/java/ru/learningByTomko/Igor/ch02task011/SolutionCh02Task011.java
@@ -0,0 +1,14 @@
+package ru.learningByTomko.Igor.ch02task011;
+
+import java.util.Scanner;
+
+public class SolutionCh02Task011 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(reversNumber(scanner.nextInt()));
+    }
+    // 2.11 Дано двузначное число. Получить число, образованное при перестановке цифр заданного числа
+    public static int reversNumber (int number){
+        return (number % 10) + (number / 100);
+    }
+}
