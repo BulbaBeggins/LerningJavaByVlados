8663c1322cc493f7f154999e4256299136001a27
BulbaBeggins <Bumerang>
2021-08-27 22:55:21 +0400

    SolutionCh02Task014

--- src/test/java/ru/learningByTomko/Igor/TestSuite.java
+++ src/test/java/ru/learningByTomko/Igor/TestSuite.java
@@ -1,11 +1,13 @@
 package ru.learningByTomko.Igor;
 
 import ru.learningByTomko.Igor.ch02task011.SolutionCh02Task011Test;
+import ru.learningByTomko.Igor.ch02task014.SolutionCh02Task014Test;
 
 public class TestSuite {
     public static void main(String[] args) {
         System.out.println("--------------");
         SolutionCh02Task011Test.main(args);
         System.out.println("--------------");
+        SolutionCh02Task014Test.main(args);
     }
 }
