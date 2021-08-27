d2d8b1e0afcfcf3f5347ba7d905d4a044b1eaa89
BulbaBeggins <Bumerang>
2021-08-27 22:28:25 +0400

    SolutionCh02Task014

---
+++ src/main/java/ru/learningByTomko/Igor/ch02task014/SolutionCh02Task014.java
@@ -0,0 +1,15 @@
+package ru.learningByTomko.Igor.ch02task014;
+
+import java.util.Scanner;
+
+public class SolutionCh02Task014 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(reversNumber(scanner.nextInt()));
+    }
+    // 2.14. Дано трехзначное число. В нем зачеркнули первую слева цифру и приписали ее в конце. Найти полученное число.
+
+    public static int reversNumber (int number){
+        return (number / 10 % 10 * 100) + (number % 100)+ (number / 100);
+    }
+}
